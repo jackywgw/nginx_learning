a7710362ab0feac13c06409215bc4c773f9723cb
jackywgw <jackywgw@126.com>
2015-09-13 20:05:31 +0800

    create nginx learning

---
+++ config.sh
@@ -0,0 +1,1 @@
+./configure --sbin-path=/usr/local/nginx/nginx --conf-path=/usr/local/nginx/nginx.conf --pid-path=/usr/local/nginx/nginx.pid --with-http_ssl_module --with-pcre=/usr/local/src/pcre-8.36 --with-zlib=/usr/local/src/zlib-1.2.8 --with-openssl=/usr/local/src/openssl-1.0.1c --with-debug
